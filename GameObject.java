433e5213743038a7f2172dce380d6acf579b7ef1
Gevin Genro Paas <55079799+ggpaas@users.noreply.github.com>
2020-04-12 17:20:32 +0300

    Mäng ver 1

---
+++ GameObject.java
@@ -0,0 +1,53 @@
+import java.awt.Graphics;
+public abstract class GameObject {
+
+    protected int x, y ;
+    protected ID id;
+    protected int velX, velY;
+
+    public GameObject(int x , int y, ID id){
+        this.x = x;
+        this.y = y;
+        this.id = id;
+    }
+
+    public abstract void tick();
+    public abstract void render(Graphics g);
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public float getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public ID getId() {
+        return id;
+    }
+
+    public void setId(ID id) {
+        this.id = id;
+    }
+
+    public float getVelX() {
+        return velX;
+    }
+
+    public void setVelX(int velX) {
+        this.velX = velX;
+    }
+
+    public float getVelY() {
+        return velY;
+    }
+
+    public void setVelY(int velY) {
+        this.velY = velY;
+    }
+
+}
